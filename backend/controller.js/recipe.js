f2d525692b7ced97c32c7587bd3b9ec3cc5ee2ec
Asen Chochev <asen.qkiqt145@gmail.com>
2025-10-14 19:38:24 +0300

    Create backend full

---
+++ backend/controller.js/recipe.js
@@ -0,0 +1,21 @@
+const getRecipes = (req, res) => {
+    res.json("БРАВО!");
+}
+
+const getRecipe = (req, res) => {
+    res.json("БРАВО!");
+}
+
+const addRecipe = (req, res) => {
+    res.json("БРАВО!");
+}
+
+const editRecipe = (req, res) => {
+    res.json("БРАВО!");
+}
+
+const deleteRecipe = (req, res) => {
+    res.json("БРАВО!");
+}
+
+module.exports = { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv').config();
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv").config()
+const connectDb=require("./config/connectionDb")
+const cors=require("cors")
 
-const PORT = 5000;
+const PORT=process.env.PORT || 3000
+connectDb()
 
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
 
-app.get('/', (req, res) =>{
-    res.json("Сървърът работи успешно");
-});
-app.listen(PORT, (err) =>{
-    console.log(`Сървърът е стартиран на порт ${PORT}`);
+app.use("/",require("./routes/user"))
+app.use("/recipe",require("./routes/recipe"))
+
+app.listen(PORT,(err)=>{
+    console.log(`app is listening on port ${PORT}`)
 })
